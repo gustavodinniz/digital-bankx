94f27686fe325074c09a382a8c75077a55e09372
gustavodiniz <gustavo.diniz@sensedia.com>
2024-01-13 17:42:31 -0300

    feat: structure initial

---
+++ src/main/java/br/com/gustavodinniz/digitalbankx/DigitalBankxApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavodinniz.digitalbankx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavodinniz/digitalbankx/DigitalBankxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavodinniz.digitalbankx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
